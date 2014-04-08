108bbb092e82982096007ae4c999e06ea9d4cd59
Greg Lee Coleman <gleeco+github@gmail.com>
2014-04-08 00:58:58 -0700

    check some sizes: raw,json,gzip,base64

---
+++ perl/try.pack_series.pl
@@ -0,0 +1,68 @@
+#!/usr/bin/env perl
+#
+#
+use JSON::XS;
+
+# estimate: max = events per day
+my $max;
+$max = 17280;     # 5 seconds 
+$max = 288;     # 5 minutes
+$max = 1440;     # 1 minute
+
+my @a;
+push @a, int rand 1024
+    for 1..$max;
+
+# test: working with quads? this didn't pan out...
+my $x = pack 'Q*', @a;      # 2384 bytes;  672 bytes gzipd
+my @z = unpack 'Q*', $x;
+
+# print $x;     # 
+my $json = JSON::XS->new->encode(\@z || []);     # 1129 bytes; 570 bytes gzipd
+my $gz = qx{ echo "$json" | gzip };
+my $enc = qx{ echo "$json" | gzip | base64 };
+
+printf( "Array tests A \t max=%d \n", $max);
+printf( "len: %d \t%s\n", length $json , 'raw json', );
+printf( "len: %d \t%s\n", length $gz, 'gzip json', );
+printf( "len: %d \t%s\n", length $enc, 'gzip json + base64', );
+
+# test: double array; [ts,val] deltas
+my @b = [ int rand 1024, int rand 1024 ];
+push @b, [
+    $b[$#b][0] - int rand 1024,
+    $b[$#b][1] - int rand 1024
+]
+    for 1..$max;
+
+$json = JSON::XS->new->encode(\@b || []);     # 1129 bytes; 570 bytes gzipd
+$gz = qx{ echo "$json" | gzip };
+$enc = qx{ echo "$json" | gzip | base64 };
+# print JSON::XS->new->encode(\@b || []);     # 1129 bytes; 570 bytes gzipd
+
+printf( "Array tests B: [tdelta,valdelta] max=%d\n", $max);
+printf( "len: %d \t%s\n", length $json , 'raw json', );
+printf( "len: %d \t%s\n", length $gz, 'gzip json', );
+printf( "len: %d \t%s\n", length $enc, 'gzip json + base64', );
+
+
+
+__END__
+
+Array tests A    max=288
+len: 1125   raw json
+len: 565    gzip json
+len: 757    gzip json + base64
+Array tests B: [tdelta,valdelta] max=288
+len: 4734   raw json
+len: 1977   gzip json
+
+
+Array tests A    max=1440
+len: 5635   raw json
+len: 2638   gzip json
+len: 3521   gzip json + base64
+Array tests B: [tdelta,valdelta] max=1440
+len: 25504  raw json
+len: 10314  gzip json
+len: 13753  gzip json + base64
